b4eec325d8ebb60112e7319be1a724d2eab276d0
drenzulli <dylanr@sas.upenn.edu>
2014-02-03 21:26:55 -0500

    Homework2 initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GooglePage::Application.config.secret_key_base = 'e0e84d80a6323455e9d507fc79f481045f4ac62d8dab09763b887120ae72545d05f7779e8695087b84857b542237a6383189208826ad23b2b07794fcdabdb72a'
